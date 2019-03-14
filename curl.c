c17f8a6eeef9e10dda02062e84857c8e8d2b724e
Freddie Wu <freddie.wu@capitaledge.cn>
2019-03-14 16:00:48 +0800

    add support for custom query headers; add doc into q script

--- curl.c
+++ curl.c
@@ -5,6 +5,7 @@
 //2019.03.13 - reformat source code
 //2019.03.14 - add support for custom query headers
 #include "./curl.h"
+#include <assert.h>
 
 volatile BOOL is_verbose = FALSE;
 
@@ -12,8 +13,7 @@ volatile BOOL is_verbose = FALSE;
  * @param x (Bool) verbose flag
  */
 K set_verbose(K x) {
-	if (!x)
-		R krr("nil");
+	assert(x);
 	switch (xt) {
 	case -KB:
 		is_verbose = !!xg;
@@ -36,23 +36,50 @@ Z size_t wr(S p, size_t s, size_t n, V*d) {
 
 #define so(c,o,v) curl_easy_setopt(c,CURLOPT_##o,v)
 
+// Dup a q string into a NULL-terminated C string.
+K dupCharList(K c) {
+	assert(c && c->t == KC && c->n >= 0);
+	K d = kpn(kC(c), c->n + 1);	//make sure string is terminated
+	kC(d)[c->n] = '\0';
+	R d;
+}
+
 /*
  * @param x (String) URL
+ * @param h (StringList) List of custom headers
  * @param y (String) POST body (NULL for GET requests)
  */
-K cpost(K x, K y) {
+K cpost(K x, K h, K y) {
 	C e[CURL_ERROR_SIZE];
 	K r, a;
-	CURL*c;
+	struct curl_slist *headers = NULL;
+	CURL *c;
 	if (xt != KC || y && y->t != KC)
 		R krr("type");
 	if (!(c = curl_easy_init()))
 		R krr("curl");
 	so(c, VERBOSE, is_verbose);
-	r = ktn(KC, 0),
+	assert(h);
+	if (h->t != 0)
+		R krr("type");
+	assert(h->n >= 0);
+	BOOL ok = TRUE;
+	DO(h->n, ok = ok && kK(h)[i] && kK(h)[i]->t == KC);
+	if (!ok)
+		R krr("type");
+	else {
+		J i = 0, _i = h->n;
+		for (; i < _i; ++i) {
+			K header = dupCharList(kK(h)[i]);
+			headers = curl_slist_append(headers, kC(header));
+			r0(header);
+		}
+	}
+	so(c, HTTPHEADER, headers);
+	r = ktn(KC, 0);
 	a = ktn(KC, xn + 1);
-	DO(xn, kC(a)[i] = kC(x)[i])
-		kC(a)[xn] = 0;
+	DO(xn, kC(a)[i] = kC(x)[i]);
+	kC(a)[xn] = 0;
 	if (y)
 		so(c, POST, 1L),
 		so(c, POSTFIELDS, kC(y)),
@@ -60,14 +87,18 @@ K cpost(K x, K y) {
 	so(c, URL, kC(a));
 	so(c, ERRORBUFFER, e);
 	so(c, FOLLOWLOCATION, 1L);
+#if 0
 	so(c, SSL_VERIFYPEER, 0L);
 	so(c, SSL_VERIFYHOST, 0L);
+#endif
 	so(c, WRITEFUNCTION, wr);
 	so(c, WRITEDATA, &r);
 	if (CURLE_OK != curl_easy_perform(c))
 		r0(r),
 		r = krr(e);
 	r0(a);
+	curl_slist_free_all(headers);
+	headers = NULL;
 	curl_easy_cleanup(c);
 	R r;
 }
@@ -75,6 +106,6 @@ K cpost(K x, K y) {
 /*
  * @see cpost
  */
-K cget(K x) {
-	R cpost(x, 0);
+K cget(K x, K h) {
+	R cpost(x, h, 0);
 }
--- qURL.q
+++ qURL.q
@@ -4,15 +4,19 @@
 DLL:`:qURL
 
 / verbose flag
+/q) .curl.verbose[]		//get current value
+/q) .curl.verbose 1b	//enable cURL verbose mode
 verbose:DLL 2:(`set_verbose;1)
 
 / HTTP GET
-/q) .curl.cget
-cget:DLL 2:(`cget;1)
+/q) .j.k .curl.cget["http://httpbin.org/headers";("Accept-Encoding: gzip";"Connection: close")]
+/q) .j.k .curl.cget["https://httpbin.org/get";()]
+cget:DLL 2:(`cget;2)
 
 / HTTP POST
-/q) .curl.cpost
-cpost:DLL 2:(`cpost;2)
+/q) .j.k .curl.cpost["https://httpbin.org/response-headers?key=value";();""]
+/q) .j.k .curl.cpost["http://httpbin.org/post";(enlist"Content-Type: text/plain");"Hello World!"]
+cpost:DLL 2:(`cpost;3)
 
 \
 __EOD__
